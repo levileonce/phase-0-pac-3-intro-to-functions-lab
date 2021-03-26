77d05579c842faa1b75b434ce10b36b41b05a34a
Levi <leviedgeleonce@gmail.com>
2021-03-26 01:43:43 -0700

    Completed assignment

---
+++ index.js
@@ -0,0 +1,32 @@
+
+function shout(string) {
+  return string.toUpperCase();
+} 
+console.log(shout("levi"))
+
+function whisper(string) {
+  return string.toLowerCase();
+} 
+console.log(whisper("LEVI"))
+
+
+
+
+function logShout(string) {
+  console.log(string.toUpperCase());
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase());
+}
+
+
+function sayHiToGrandma(string) {
+  if (string === string.toLowerCase(string)) {
+    return ("I can't hear you!")
+  } if (string === string.toUpperCase(string)) {
+     return ("YES INDEED!")
+  } if (string === "I love you, Grandma.") {
+  return ("I love you, too.")
+ }
+}
